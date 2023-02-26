2f814cfd621ebc2b077e00e51b90bd96ec395e9c
canyener <can.yener@hotmail.com.tr>
2023-02-26 16:16:24 +0300

    :sparkles: AttendToTravelPlan command, handler and validator

---
+++ src/Application/Features/TravelPlans/Commands/AttendToTravelPlan/AttendToTravelPlanResponseDTO.cs
@@ -0,0 +1,8 @@
+namespace Application.Features.TravelPlans.Commands.AttendToTravelPlan;
+
+public class AttendToTravelPlanResponseDTO
+{
+  public int TravelPlanId { get; set; }
+  public int StatusCode { get; set; }
+  public string Message { get; set; }
+}
